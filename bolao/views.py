ef3fdd9d7badb0880aeb2cacc0f48b891800b1b8
Roney Santos <roneylira@hotmail.com>
2020-01-14 20:01:07 -0300

    another test with initial HTML

--- bolao/views.py
+++ bolao/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Jogo, Aposta
 
 # Create your views here.
 
 def jogos_list(request):
-	return render(request, 'bolao/jogos_list.html', {})
+	jogos = Jogo.objects.all()
+	return render(request, 'bolao/jogos_list.html', {'jogos': jogos})
