5e2d3a29d35755321084d6dc5865f3c20ab65131
Roney Santos <roneylira@hotmail.com>
2020-01-14 20:24:05 -0300

    adding the empty resultados and rodadas_anteriores html files

--- bolao/urls.py
+++ bolao/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.jogos_list, name='jogos_list'),
+    path('classificacao/', views.classificacao, name='classificacao'),
+    path('rodadas_anteriores/', views.rodadas_anteriores, name='rodadas_anteriores'),
 ]
--- bolao/views.py
+++ bolao/views.py
@@ -6,3 +6,9 @@ from .models import Jogo, Aposta
 def jogos_list(request):
 	jogos = Jogo.objects.all()
 	return render(request, 'bolao/jogos_list.html', {'jogos': jogos})
+
+def classificacao(request):
+	return render(request, 'bolao/classificacao.html', {})
+
+def rodadas_anteriores(request):
+	return render(request, 'bolao/rodadas_anteriores.html', {})
